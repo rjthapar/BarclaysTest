4f9fab276dd072a81162182ee521b9698c80d503
rajat.thapar <rajat.thapar@outlook.com>
2021-10-10 16:08:15 +0530

    Barclays Test changes

--- FileData/Program.cs
+++ FileData/Program.cs
@@ -9,6 +9,31 @@ namespace FileData
     {
         public static void Main(string[] args)
         {
+            try
+            {
+                string[] read = Console.ReadLine().Split(' ');
+                string prefix = read[0];
+                string file = read[1];
+                FileDetails fd = new FileDetails();
+
+                if (prefix.In('-v', '--v', '/v', '--version'))
+                {
+                    Console.WriteLine(fd.Version(file));
+                }
+                else if (prefix.In('-s', '--s', '/s', '--size'))
+                {
+                    Console.WriteLine(fd.Size(file));
+                }
+                else
+                {
+                    Console.WriteLine("Enter correct values");
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex);
+            }
+            
         }
     }
 }
